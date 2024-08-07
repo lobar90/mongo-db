efb31b4b3c70ee79f15ef2ac5df8eec22bd23ce4
Lobar <lobar90@inbox.ru>
2024-08-07 16:02:40 +0300

    добавил миграции, изменил deviceId на _id у участка и обородования, здесь будут лежать Object как и монге

--- src/migrations/1723027807655_devices.ts
+++ src/migrations/1723027807655_devices.ts
@@ -1,9 +1,24 @@
 import { Db } from 'mongodb'
 import { MigrationInterface } from 'mongo-migrate-ts';
 import { IDevice } from '../interfaces/device'
+import { ObjectId } from 'mongodb';
 
 export class devices1723027807655 implements MigrationInterface {
   public async up(db: Db): Promise<any> {
+    const idString = "66b36421e2f55043d4ca48df";
+const newId = new ObjectId(idString);
+    const migrationData: IDevice[] = [
+      {
+        _id: newId,
+        deviceName: 'Printer',
+        deviceType: 'printer',
+        connectionParameters: {
+          ip: 'http://127.0.0.1',
+          port: '82000'
+        }
+      },
+    ]
+    await db.collection('devices').insertMany(migrationData)
   }
 
   public async down(db: Db): Promise<any> {
--- src/migrations/1723027816554_sections.ts
+++ src/migrations/1723027816554_sections.ts
@@ -1,10 +1,30 @@
 import { Db } from 'mongodb'
 import { MigrationInterface } from 'mongo-migrate-ts';
 import { ISection } from '../interfaces/section'
+import { ObjectId } from 'mongodb';
 
 export class sections1723027816554 implements MigrationInterface {
   public async up(db: Db): Promise<any> {
+    const idString = "66b36d03cf1edc304d3adf25";
+    const newId = new ObjectId(idString);
+    const migrationData: ISection[] = [
+      {
+        _id: newId,
+        sectionType: 'serilization',
+        sectionName: 'Участок сериализации',
+        businessProcess: '01',
+        devices: [
+          {
+            deviceId: '66b36421e2f55043d4ca48df',
+            deviceType: 'printer'
+          }
+        ]
+      },
+    ]
+
+    await db.collection('sections').insertMany(migrationData)
   }
+
 
   public async down(db: Db): Promise<any> {
     await db.collection('sections').deleteMany({})
